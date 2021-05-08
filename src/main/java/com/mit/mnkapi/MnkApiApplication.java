ded59a7797f370e07f00547dfb56f7e39ffcb468
Nipuna <mnipunai7@gmail.com>
2021-05-08 20:47:38 +0530

    first commit

---
+++ src/main/java/com/mit/mnkapi/MnkApiApplication.java
@@ -0,0 +1,13 @@
+package com.mit.mnkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnkApiApplication.class, args);
+	}
+
+}
